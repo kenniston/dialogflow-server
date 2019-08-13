e946e46d7114c0a3735fb4283494abacbe8421df
C1280932 Kenniston Arraes Bonfim <c1280932@bb.com.br>
2019-08-13 18:06:36 -0300

    Heroku base config

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(compression())
 
-app.listen(3000, () => {
-  console.log(`Server listening on port 3000.`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
 });
 
 app.use("/api", routes);
